fe6ca586a97616a2b16dd5bdb62ae3726596f2a6
AlinaAlexandraVizireanu <105154646+AlinaAlexandraVizireanu@users.noreply.github.com>
2022-09-24 20:46:17 +0300

    Changing the flex value

--- Expanding Cards/script.js
+++ Expanding Cards/script.js
@@ -1,14 +1,16 @@
 const panels = document.querySelectorAll('.panel');
 
-for(let panel of panels){
-    panel.addEventListener('click', function(){
+
+for (let panel of panels) {
+    panel.addEventListener('click', function () {
         removeActiveClasses();
-        panel.classList.add('active');    
+        panel.classList.add('active');
     });
 }
 
-function removeActiveClasses(){
-    for(let panel of panels){
+function removeActiveClasses() {
+    for (let panel of panels) {
         panel.classList.remove('active');
     }
 }
+
